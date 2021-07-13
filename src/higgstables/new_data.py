fc9ed1570fb7a0c2446efe9d986c599d146213da
Jonas Kunath <jonas.kunath@gmx.de>
2021-07-13 09:58:21 +0200

    Bug fix: Allow running the script from anywhere

--- src/higgstables/new_data.py
+++ src/higgstables/new_data.py
@@ -9,11 +9,20 @@ rootfile_help = (
 )
 
 if __name__ == "__main__":
+    try:
+        default_csv_dir = Path(__file__).parents[2] / "data"
+    except IndexError:
+        # This script might not be that far down in the file hierarchy.
+        default_csv_dir = Path("data")
     import argparse
+
     parser = argparse.ArgumentParser(description="Replace the data.")
     parser.add_argument("rootfile", help=rootfile_help)
-    parser.add_argument("--data_dir", type=str,
+    parser.add_argument(
+        "--data_dir",
+        type=str,
         help="Folder to store data into.",
-        default=Path(__file__).parents[2] / "data")
+        default=default_csv_dir,
+    )
     args = parser.parse_args()
     save_data(args.rootfile, args.data_dir)
