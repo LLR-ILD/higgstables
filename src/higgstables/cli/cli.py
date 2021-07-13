cf0d8b50a69eb6bdcffe516fff2215f57cfad9e3
Jonas Kunath <jonas.kunath@gmx.de>
2021-07-13 19:47:46 +0200

    Add checks to data_dir in CLI

--- src/higgstables/cli/cli.py
+++ src/higgstables/cli/cli.py
@@ -1,4 +1,5 @@
 """The higgstables command line interface."""
+import argparse
 from pathlib import Path
 
 import higgstables
@@ -8,14 +9,22 @@ from ..make_data import save_data
 rootfile_help = "The basic file with simulated events."
 
 
-def main():
-    try:
-        default_csv_dir = Path(__file__).parents[2] / "data"
-    except IndexError:
-        # This script might not be that far down in the file hierarchy.
-        default_csv_dir = Path("data")
-    import argparse
+def data_to_dir(data_dir):
+    data_dir = Path(data_dir)
+    if data_dir.is_dir():
+        is_empty = not any(data_dir.iterdir())
+        if is_empty:
+            return data_dir
+        else:
+            raise FileExistsError(
+                f"{data_dir.absolute()} already exists and is non-empty. "
+                "Please provide a new path for data output through `-d new_dir`."
+            )
+    data_dir.mkdir(parents=True)
+    return data_dir
 
+
+def main():
     parser = argparse.ArgumentParser(
         description=higgstables.__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -25,10 +34,11 @@ def main():
     )
     parser.add_argument("rootfile", help=rootfile_help)
     parser.add_argument(
+        "-d",
         "--data_dir",
-        type=str,
-        help="Folder to store data into.",
-        default=default_csv_dir,
+        type=data_to_dir,
+        help="Folder to store tables into.",
+        default="data",
     )
     args = parser.parse_args()
     save_data(args.rootfile, args.data_dir)
